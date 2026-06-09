59673761a16657359aaed76a9c3695576fe4c818
SaraF <sara.formoso.prt@msft.cesae.pt>
2026-06-09 14:49:08 +0100

    Criação do SQLiteTestHelper, com metodo de criação da base de dados in-memory, e criação da tabela com retorno da connetion ( ligação aberta)

---
+++ TestesProjetoxUnit/Helper/SQLiteTestHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestesProjetoxUnit.Helper
+{
+    public class SQLiteTestHelper
+    {
+
+        //Criação de uma base de dados limpa,
+        // in-memory (não cria ficheiro)
+        //com tabela já pronta a usar
+
+        public static SqliteConnection CreateInMemoryDataBase()
+        {
+            var connetion = new SqliteConnection("DataSource=:memory:");
+
+            connetion.Open();
+
+            //criar a tabela
+
+            var cmd = connetion.CreateCommand();
+            cmd.CommandText = @"CREATE TABLE Produtos(Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nome TEXT NOT NULL, Preco REAL NOT NULL);";
+
+            cmd.ExecuteNonQuery();
+
+            return connetion;
+
+            //este return devolve a ligação aberta e com a tabela criada
+
+        }
+    }
+}
